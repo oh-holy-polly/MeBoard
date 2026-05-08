4a30fcd17b5812af1b92c1e242489593817508f9
oh-holy-polly <polina.ppiletskaya@gmail.com>
2026-05-08 12:23:39 +0300

    Add useMemo import to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { supabase } from '@/lib/supabaseClient'
 import { motion } from 'framer-motion';
 import { 
